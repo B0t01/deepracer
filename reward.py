8d55342e53289c39f9f4696394e6471de35a3ed8
b0t01 <ayushnagarkoti2005@gmail.com>
2024-02-19 20:11:16 +0530

    updated reward funtion

--- reward.py
+++ reward.py
@@ -1,2 +1,13 @@
 def reward_func(params):
-    pass
+
+    track_width = params["track_width"]
+    distance_from_center = params["distance_form_center"]
+
+    distance_from_border = 0.5*track_width - distance_from_center
+
+    if distance_from_border >= 0.1:
+        reward=1
+
+    else:
+        reward=1e-3
+
